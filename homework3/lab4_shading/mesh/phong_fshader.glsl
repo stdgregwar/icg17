2335766740c2b5f909fbdb0bedc3408ff2cb23dd
Gregoire Hirt <ghirt@icin3pc16.epfl.ch>
2017-03-16 14:32:45 +0100

    phong shading

--- homework3/lab4_shading/mesh/phong_fshader.glsl
+++ homework3/lab4_shading/mesh/phong_fshader.glsl
@@ -1,21 +1,23 @@
 #version 330
 
+uniform vec3 La;
+uniform vec3 Ld;
+uniform vec3 Ls;
+
+uniform vec3 ka;
+uniform vec3 kd;
+uniform vec3 ks;
+
+in vec3 normal_mv;
+in vec3 light_dir;
+in vec3 view_dir;
 
 out vec3 color;
 
-
 void main() {
-    color = vec3(0.0,0.0,0.0);
-
-    const vec3 COLORS[6] = vec3[](
-        vec3(1.0,0.0,0.0),
-        vec3(0.0,1.0,0.0),
-        vec3(0.0,0.0,1.0),
-        vec3(1.0,1.0,0.0),
-        vec3(0.0,1.0,1.0),
-        vec3(1.0,0.0,1.0));
-    int index = int( mod(gl_PrimitiveID,6) );
-    color = COLORS[index];
+    vec3 normal = normalize(normal_mv);
+    vec3 light = normalize(light_dir);
+    vec3 view = normalize(view_dir);
 
     //>>>>>>>>>> TODO >>>>>>>>>>>
     // TODO 1.2: Phong shading.
@@ -26,4 +28,8 @@ void main() {
     // the code above after
     // implementing Phong shading.
     //<<<<<<<<<< TODO <<<<<<<<<<<
+    float diff = clamp(dot(normal,light),0,1);
+    vec3 ref = reflect(light,normal);
+    float spec = pow(clamp(dot(ref,view),0,1),2);
+    color = La*ka+Ld*kd*diff+Ls*ks*spec;
 }
